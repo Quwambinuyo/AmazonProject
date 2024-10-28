51a0c61f5cbf125400414bae074574e47b348095
myAmazonProject <qbinuyo7@gmail.com>
2024-10-28 16:17:40 +0100

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -79,6 +79,16 @@ document.querySelectorAll(".js-add-to-cart").forEach(function (button) {
         quantity: 1,
       });
     }
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+
+    console.log(cartQuantity);
     console.log(cart);
   });
 });
