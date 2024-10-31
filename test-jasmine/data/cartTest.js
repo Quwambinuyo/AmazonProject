b22f6afcf9314d6a3c4526d09dcb9de68fd32aab
myAmazonProject <qbinuyo7@gmail.com>
2024-10-31 08:49:01 +0100

    DId testing for cart page and order summary

--- test-jasmine/data/cartTest.js
+++ test-jasmine/data/cartTest.js
@@ -1,4 +1,5 @@
 import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
+import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 
 describe("test suite: addToCart", () => {
   it("adds an existing product to the cart", () => {
@@ -30,9 +31,38 @@ describe("test suite: addToCart", () => {
     loadFromStorage();
 
     addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
-    expect(cart.length).toEqual(1);
+    expect(cart.length).toEqual(2);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-    expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    expect(cart[0].productId).toEqual("15b6fc6f-327a-4ec4-896f-486349e85a3d");
     expect(cart[0].quantity).toEqual(1);
+  });
+
+  it("remove a product", () => {
+    document.querySelector(".js-test-container").innerHTML = `
+    <div class="js-order-summary"></div>  
+    `;
+
+    const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
+    const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([
+        {
+          productId: productId1,
+          quantity: 2,
+          deliveryOptionId: "1",
+        },
+
+        {
+          productId: productId2,
+          quantity: 1,
+          deliveryOptionId: "2",
+        },
+      ]);
+    });
+
+    loadFromStorage();
+
+    renderOrderSummary();
   });
 });
